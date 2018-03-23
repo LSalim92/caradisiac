6700fe85e66a2c5f92658a9d87f3933bc23f73e7
Salim <salim.laabi@hotmail.fr>
2018-03-23 10:51:55 +0100

    Indexing with elasticsearch

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
+var elasticsearch = require('elasticsearch');
 var fs = require('fs');
 //var elasticsearch = require('elasticsearch');
 
+var client = new elasticsearch.Client({
+  host: 'localhost:9292',
+  log: 'trace'
+});
 
 async function getBrand(){
   const brands = await getBrands();
+  //console.log(brands);
   return brands;
-  console.log(brands);
 }
 
-// var allcaradisiac ={};
 async function getModel(string){
   const models = await getModels(string);
-  console.log(models);
-  // allcaradisiac.push(models);
-  // var json = JSON.stringify(allcaradisiac);
-  // fs.writeFile('caradisiac.json', json, 'utf8');
+  //console.log(models);
+  return models;
 }
 
 let brands = getBrand();
-// console.log(brands);
+let models = getModel('PEUGEOT');
+
+//console.log(brands);
 
 brands.then(function(result){
-  // console.log(result);
+  details = {}
   for (var i = 0; i < result.length; i++) {
-    details = getModel(result[i]);
+    //details = getModel(result[i]);
+    details.push(getModel(result[i]));
   }
+  console.log(details);
+  var json = JSON.stringify(details);
+  fs.writeFile('caradisiac.json', json, 'utf8');
+});
+
+client.bulk({
+  body: [
+    // action description
+    { index:  { _index: 'car', _type: 'string', _id: 1 } },
+     // the document to index
+    { title: 'json' },
+  ]
+}, function (err, resp) {
+  console.log("ERROR");
 });
